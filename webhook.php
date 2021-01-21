4901a55cdd15226ea843ad5daad0c02e6c20d3f8
KoseiTanabe <71837164+KoseiTanabe@users.noreply.github.com>
2021-01-22 01:46:28 +0900

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -18,8 +18,8 @@
 
 require_once('LINEBotTiny.php');
 
-$channelAccessToken = 'XTUXbtcyMfHjFNW/4U5c23i+h82Jpk22BfVfI37tH7PHZuMiQ5m65pNubFfEZkW0JbHYO/DZOmdEnaOh2YBsa1Zp6+lXtWF5ItdkwpLbUhIY//dN5F8KQezktXG4igFg6mkqaWXNwnjScsV6rir/OAdB04t89/1O/w1cDnyilFU=';
-$channelSecret = '4806448192de8cb41ccf3d2857b92e07';
+$channelAccessToken = 'xxxxxxx';
+$channelSecret = 'xxxxxxxxxx';
 
 //LinebotTinyクラスのインスタンスを生成し、$clientに格納する
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
